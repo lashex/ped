1b3d9dce67203cb78324a69400765a68cca5f947
Brett <brett@gridward.com>
2012-12-14 22:33:51 -0800

    added a couple more pieces of metadata and tweaked to be in line with recommended package file structure

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+import os
 from distutils.core import setup
 from setuptools import find_packages
 
+version = '0.1.0'
+README = os.path.join(os.path.dirname(__file__), 'README.txt')
+long_description = open(README).read() + 'nn'
+
 setup(name='plantextract',
-      version='0.1.0',
+      version=version,
       description='Python Plant Extract Document processor',
+      long_description = long_description,
       author='Brett Francis',
+      url='https://github.com/lashex/ped.git',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["lxml >= 3.0"],
+      install_requires=['lxml >= 3.0'],
       keywords = "sunspec plant extract",
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Other Audience',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities',],
       )
