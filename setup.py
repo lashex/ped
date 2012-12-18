1df72d75d1a3295bc2ee3c3145cf9d2d5310d8e8
Brett <brett@gridward.com>
2012-12-18 00:04:11 -0800

    first round of getting ts_calc working

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
-version = '0.2.1'
-README = os.path.join(os.path.dirname(__file__), 'README.txt')
+version = '0.2.2'
+README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read() + 'nn'
 
 setup(name='plantextract',
