e9639e7904e7ec475897d8a3439e869dd2f94254
Brett <brett@gridward.com>
2012-12-17 22:16:00 -0800

    fixed a (big) flub left over from merge

--- plantextract/sunspec_data.py
+++ plantextract/sunspec_data.py
@@ -232,160 +232,17 @@ class Model(object):
 
     for sp in man_points.itervalues():
       if sp.mandatory:
-#        found = False
+        found = False
         for p in points:
-            if (end_time is None) and (start_time is None):
-                period_points.append(p)
-            elif start_time < p.time < end_time:
-                period_points.append(p)
-        logging.info('Period points:', str(period_points))
-        period_points = sorted(period_points, key=attrgetter('time'))
-        return period_points
+            if sp.id == p.id:
+                logging.info("Found mandatory point:" + str(p.id))
+                found = True
+                break
 
-    def tostring(self):
-        return ''.join(['SunSpecData v:', str(self.version),
-                        ' point count: ', str(len(self.get_points()))])
-
-
-class DeviceRecord(object):
-    element_name = 'd'
-
-    def __init__(self, element):
-        element = element
-        if element is None:
-            logging.warning("DeviceRecord.__init__() element provided is None")
-            return
-
-        self.manufacturer = get_attr(element, 'man')
-        self.model = get_attr(element, 'mod')
-        self.serial_number = get_attr(element, 'sn')
-        self.logger_id = get_attr(element, 'lid')
-        self.logger_id_namespace = get_attr(element, 'ns')
-        self.device_interface_id = get_attr(element, 'if')
-        self.device_id = get_attr(element, 'id')
-        self.correlation_id = get_attr(element, 'cid')
-
-        self._determine_id()
-        time = get_attr(element, 't')
-        if time is None:
-            raise SunSpecDataException("DeviceRecord 't' attribute is required.")
-        else:
-            # SunSpecData <d ... t="YYYY-MM-DDThh:mm:ssZ"
-            self.time = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
-
-        self.models = list()
-        for m in element.iter(Model.element_name):
-            m_id = m.get('id')
-            if (m_id is not None):
-                logging.debug("DeviceRecord.__init__() adding model_id: " + m_id)
-                self.models.append(Model(m, m_id, self.time))
-
-    def _determine_id(self):
-        self.device_id_type = None
-        gs = None
-        if self.model and self.manufacturer and self.serial_number:
-            gs = "" + self.serial_number + self.model + self.manufacturer
-            self.device_id_type = 'Common Block'
-        elif self.logger_id and self.device_id:
-            gs = "" + self.device_id + self.logger_id
-            self.device_id_type = 'Logger-specific, user-assigned'
-
-        if gs is None:
-            s = "A Device must have a globally unique identifier, either man: mod: sn: or lid: id:"
-            raise SunSpecDataException(s)
-
-        self.guid = int(hashlib.md5(gs).hexdigest(), 16)
-        return
-
-    def parse(self):
-        logging.debug("SunSpecData.DeviceRecord.parse()")
-        for model in self.models:
-            model.parse()
-
-    def tostring(self):
-        return ''.join(["Device", os.linesep, " id_type:", self.device_id_type, " guid:",
-                        str(self.guid), os.linesep, " man:", self.manufacturer,
-                        " mod:", self.model, " sn:", self.serial_number, os.linesep,
-                        " lid:", self.logger_id, " ns:", self.logger_id_namespace,
-                        " id:", self.device_id, os.linesep, " if:",
-                        self.device_interface_id, " cid:", self.correlation_id])
-
-
-class Model(object):
-    element_name = 'm'
-
-    def __init__(self, element, id, dr_time):
-        self.dr_time = dr_time
-        self.points = list()
-        if (element is None):
-            raise SunSpecDataException("SunSpec model element is required for a Model")
-        else:
-            self.element = element
-
-        if (id is None):
-            raise SunSpecDataException("SunSpec model element must have an id attribute")
-        else:
-            self.id = int(id)
-
-        self.smdx = SMDX(element, id)
-
-    def parse(self):
-        logging.debug("Model.parse() model_id:" + str(self.id))
-
-        logging.info("Model.parse() instantiating Points")
-        for p in self.element.iter(Point.element_name):
-            self.points.append(Point(self, p))
-
-        smdx_points = self.smdx.get_points()
-        logging.info("Model.parse() adding units from SMDXPoints to Points")
-        for p in self.points:
-            if p.id in smdx_points:
-                logging.info("Point.id:" + str(p.id) + " found in SMDX.model_id: " + str(self.id))
-                p.units = smdx_points[p.id].units
-                p.set_type(smdx_points[p.id].type)
-                logging.info("Point.id:" + str(p.id) + " assigned units: " + str(p.units) +
-                             " type:" + str(p.type))
-            else:
-                logging.warning(''.join(["Point.id:", str(p.id),
-                                " exists but shouldn't for SMDX.model_id: ", str(self.id)]))
-
-        mand_met = False
-        if (len(self._has_mandatory_points()) == 0):
-            mand_met = True
-        logging.debug("Model.parse() has all mandatory points? " + str(mand_met))
-
-    def get_matching_points(self, point_id):
-        '''Get a list of points which match the given point_id
-        '''
-        points = list()
-        logging.info(''.join(["Model.get_matching_points() looking for point_id:",
-                     str(point_id)]))
-        for p in self.points:
-            logging.debug(''.join(["Model.get_matching_points() p.id:", p.id,
-                                   " given point_id:", str(point_id)]))
-            if p.id == point_id:
-                points.append(p)
-        return points
-
-    def _has_mandatory_points(self):
-        man_points = self.smdx.get_mandatory_points()
-        points = self.points
-        logging.debug("Model._has_mandatory_points()")
-        missing_mand = {}
-
-        for sp in man_points.itervalues():
-            if sp.mandatory:
-                found = False
-                for p in points:
-                    if sp.id == p.id:
-                        logging.info("Found mandatory point:" + str(p.id))
-                        found = True
-                        break
-
-                if not found:
-                    logging.warning("Model.id:" + str(self.id) +
-                                    " missing mandatory SMDXPoint.id:" + sp.id)
-                    missing_mand[sp.id] = sp
+        if not found:
+            logging.warning("Model.id:" + str(self.id) +
+                            " missing mandatory SMDXPoint.id:" + sp.id)
+            missing_mand[sp.id] = sp
         return missing_mand
 
     def set_metadata(self, element):
@@ -400,7 +257,6 @@ class Point(object):
 
     def __init__(self, model, element):
         self.model = model
-        element = element
         self.id = element.get('id')
         self.x = element.get('x')
         self.value = element.text
