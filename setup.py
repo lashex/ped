d903c6942a59a33f938c9f9549e3da5a07a6b0b9
Brett Francis <brett@gridward.com>
2013-06-18 00:47:29 -0700

    prepping for pypi package rebuild

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
-version = '0.3.2'
+version = '0.3.0'
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read()
 
