833e7e18a912a9dafff3d9e3a33bf5691bf723f1
Brett Francis <brett@gridward.com>
2013-10-28 23:47:59 -0700

    fleshed out deeper assertions into PyXB parsed XML

--- plantextract/test_ped.py
+++ plantextract/test_ped.py
@@ -42,22 +42,48 @@ class PedObjectTest(PedTest):
             )
         )
 
-        print ped
-        print ped.plant
-        print ped.plant.location
-        print ped.plant.name_plate
-        print ped.plant.equipment
+        #print ped
+        #print ped.plant
+        #print ped.plant.location
+        #print ped.plant.name_plate
+        #print ped.plant.equipment
         self.assertEqual("2013-03-02", ped.plant.activation_date)
 
 
 class PlantExtractParserTestCase(PedTest):
-    def test_parse(self):
-        parser = pedparser.PlantExtractParser()
+    def test_large_parse(self):
         this_dir, this_filename = os.path.split(__file__)
         ped_file = os.path.join(this_dir, 'examples', 'ped-large.xml')
+        parser = pedparser.PlantExtractParser()
         parser.parse(ped_file=ped_file)
         self.assertEqual("Beach City", parser.ped.plant.location.city)
+        ssd = parser.ped.sunSpecData
+        for device in ssd.d:
+            for model in device.m:
+                if model.id is '1':
+                    for point in model.p:
+                        if point.id is 'Mn':
+                            self.assertEqual('Amalgamated Industries',
+                                             point.value())
+                        elif point.id is 'Md':
+                            self.assertEqual('Composite SuperDevice',
+                                             point.value())
 
+    def test_moxa_parse(self):
+        this_dir, this_filename = os.path.split(__file__)
+        ped_file = os.path.join(this_dir, 'examples', 'moxa-americas-ped.xml')
+        parser = pedparser.PlantExtractParser()
+        parser.parse(ped_file=ped_file)
+        plant = parser.ped.plant
+        self.assertEqual("Moxa Americas HQ Solar Plant", plant.name)
+        self.assertEqual("2011-03-22", plant.activationDate)
+        for property in plant.namePlate.property_:
+            if property.id is 'installedDCCapacity':
+                self.assertEqual(311.75, property.value())
+            elif property.id is 'installedACCapacity':
+                self.assertEqual(272.571, property.value())
+            elif property.id is 'installedPanelArea':
+                self.assertEqual(2198, property.value())
 
 def suite():
     suite = unittest.TestSuite()
